9f6fe67dccca25d318ae8f9f30f32adf04c7df84
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-04-03 20:56:48 +0200

    worked updata user without redux

--- src/axios/UserProfile.jsx
+++ src/axios/UserProfile.jsx
@@ -1,12 +1,24 @@
 import axiosInstance from "./config";
 
-// axiosInstance.defaults.xsrfCookieName = "csrftoken";
-// axiosInstance.defaults.xsrfHeaderName = "X-CSRFToken";
-// axiosInstance.defaults.withCredentials = true;
-
 export const getUserById = (userId) => {
   return axiosInstance.get(`/users/user/${userId}/`);
 };
+// export const updateUserById = (userId) => {
+//   return axiosInstance.patch(`/users/user/${userId}/`, {
+//     // params: updatedUserData,
+//     params: { first_name: "Hesham" },
+//   });
+// };
+
+export const updateUserById = (userId, updatedUserData) => {
+  return axiosInstance.patch(`/users/user/${userId}/`, updatedUserData);
+};
+
+// console.log(
+//   `From updateUserById Axios: userId : ${userId} , updatedUserData: ${JSON.stringify(
+//     updatedUserData
+//   )} `
+// );
 
 // .get("https://dummyjson.com/users/search", {
 //   params: { q: "John" },
--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,50 +1,82 @@
 import { useState, useEffect } from "react";
-// import { getUserById } from "./../../axios/UserProfile.jsx";
-import { useDispatch, useSelector } from "react-redux";
-
-import { getLoggedUserThunk } from "./../../store/slices/userProfileSlice";
+import { getUserById, updateUserById } from "./../../axios/UserProfile.jsx";
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//   getLoggedUserThunk,
+//   updateUserThunk,
+// } from "./../../store/slices/userProfileSlice";
 
 const UserProfile = () => {
-  // const userId = 1;
-  // const [userData, setUserData] = useState({});
+  const userId = 1;
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    getUserById(userId)
+      .then((res) => {
+        console.log("getUserById data", res.data);
+        setUserData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  function updateUser() {
+    updateUserById(userId, { first_name: "Hesham" })
+      .then((res) => {
+        console.log("updated data", res.data);
+        setUserData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   // useEffect(() => {
   //   getUserById(userId)
   //     .then((res) => {
   //       console.log("res", res.data);
+
   //       setUserData(res.data);
   //     })
   //     .catch((err) => {
   //       console.log(err);
   //     });
-  // }, []);
-  // useEffect(() => {
-  //   if (userData) {
-  //     console.log("userData: ", userData);
-  //   }
   // }, [userData]);
 
-  const dispatch = useDispatch();
-  const loggedUser = useSelector((state) => {
-    return state.userReducer.LoggedUser;
-  });
-  // const isLoading = useSelector((state) => state.userReducer.isLoading);
+  // const dispatch = useDispatch();
+  // const loggedUser = useSelector((state) => state.userReducer.LoggedUser);
 
-  useEffect(() => {
-    dispatch(getLoggedUserThunk());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getLoggedUserThunk(userId));
+  // }, [dispatch]);
+  // console.log("loggedUser:", loggedUser);
 
-  console.log("loggedUser:", loggedUser);
-
+  // const updateUserProfile = async () => {
+  //   const updatedUserData = {
+  //     first_name: "Rahma",
+  //   };
+  //   try {
+  //     console.log("Dispatching updateUserThunk action");
+  //     dispatch(updateUserThunk(userId));
+  //     console.log("Update dispatch successful");
+  //   } catch (error) {
+  //     console.error("Error updating user profile:", error);
+  //   }
+  // };
   return (
     <div className="container mt-3">
       <h1>User Data </h1>
       <div className="row mt-3">
-        {/* <ul>
+        <ul>
           <li> {userData.first_name}</li>
           <li> {userData.last_name}</li>
           <li> {userData.phone}</li>
-        </ul> */}
+        </ul>
       </div>
+      <button className="btn" onClick={updateUser}>
+        Update Profile
+      </button>
     </div>
   );
 };
