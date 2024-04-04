a1780cd0718d755874b670c8be0c29b1a93a63e3
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-04-05 01:44:10 +0200

    new get user orders , get order details (without redux)

--- src/axios/UserOrders.jsx
+++ src/axios/UserOrders.jsx
@@ -1,5 +1,13 @@
 import axiosInstance from "./config";
 
+// export const getUserOrders = (userId) => {
+//   return axiosInstance.get(`orders/userorder/${userId}/`);
+// };
+
 export const getUserOrders = (userId) => {
-  return axiosInstance.get(`orders/userorder/${userId}/`);
+  return axiosInstance.get(`users/user/${userId}/orders/`);
+};
+
+export const getOrderDetails = (userId, orderId) => {
+  return axiosInstance.get(`users/user/${userId}/orders/order/${orderId}/`);
 };
--- src/pages/UserOrders/UserOrders.jsx
+++ src/pages/UserOrders/UserOrders.jsx
@@ -1,9 +1,12 @@
-import { useEffect } from "react";
-// import { getUserOrders } from "../../axios/UserOrders.jsx";
+import { useState, useEffect } from "react";
+import { getOrderDetails } from "../../axios/UserOrders.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserOrdersThunk } from "./../../store/slices/userOrdersSlice";
 const UserOrders = () => {
   const userId = 1;
+  const orderId = 1; // Example order ID
+  const [orderDetails, setOrderDetails] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const dispatch = useDispatch();
   const userOrders = useSelector((state) => {
@@ -15,6 +18,24 @@ const UserOrders = () => {
   }, [dispatch]);
 
   console.log("userOrders:", userOrders);
+
+  const fetchOrderDetails = async () => {
+    setLoading(true);
+    try {
+      const response = await getOrderDetails(userId, orderId);
+      setOrderDetails(response.data);
+    } catch (error) {
+      console.error("Error fetching order details:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (orderDetails.length != 0) {
+      console.log("orderDetails", orderDetails);
+    }
+  }, [orderDetails]);
+
   return (
     <div className="container mt-3">
       <h1>User Orders </h1>
@@ -24,7 +45,25 @@ const UserOrders = () => {
             <p>Order ID: {order.order_id}</p>
             <p>Date: {order.order_date}</p>
             <p>Amount: {order.total_amount}</p>
-            {/* Add more fields as needed */}
+            <div className="row mt-3">
+              {loading && <p>Loading...</p>}
+              {orderDetails.length != 0 && (
+                <ul>
+                  {orderDetails.map((order, index) => (
+                    <li key={index}>
+                      <ul>
+                        <li>Product Name: {order.product.name}</li>
+                        <li>product_price: {order.product_price}</li>
+                        <li>Quantity: {order.quantity}</li>
+                      </ul>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+            <button className="btn" onClick={fetchOrderDetails}>
+              see order details
+            </button>
           </div>
         ))}
       </div>
