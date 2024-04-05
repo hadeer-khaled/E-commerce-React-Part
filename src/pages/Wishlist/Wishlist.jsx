bad0767c9f6114046641157fd8ce88c671369399
Nooran Tarek <noort.mohamed@gmail.com>
2024-04-06 01:34:58 +0200

    adding trash icon

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchWishlistItemsThunk } from '../../store/slices/userShowWishlistSlice';
 import { removeProductFromWishlist } from '../../axios/UserWishlist';
 
+
 export default function Wishlist() {
     const dispatch = useDispatch();
     const wishlistItems = useSelector((state) => state.userWishlistReducer.wishlistItems);
@@ -71,10 +72,14 @@ export default function Wishlist() {
       </button>
     </td>
     <td>
-      <button className="btn" onClick={() => handleClick(product.product_id)}>
-        Remove
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-      </button>
+    <button className="btn" onClick={() => handleClick(product.product_id)}>
+  Remove
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+  </svg>
+</button>
+
+
       {/* Alert */}
       {showAlert && (
         <div className="toast toast-bottom toast-end">
