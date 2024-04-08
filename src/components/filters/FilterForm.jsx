0801a8c0869b7680aa159b30f0b4bc32260e1588
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-04-08 18:47:58 +0200

    handle filtering in shop page

--- src/components/filters/FilterForm.jsx
+++ src/components/filters/FilterForm.jsx
@@ -78,7 +78,7 @@ const FilterForm = ({ onSubmit, initialCategory }) => {
             onChange={handleChange}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           >
-            <option value="">Filter</option>
+            <option value="">Sort by</option>
             <option value="-avg_rating">Price: High rated first</option>
             <option value="price">Price: low prices first</option>
             <option value="-price">Rating: High prices first</option>
